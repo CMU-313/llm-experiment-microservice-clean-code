5bccdeb7023b27dfdf7434720e492089e27b3ebb
topshenyi-web <erxu@ctemc.org>
2026-03-26 18:54:42 -0400

    chore: upd model

--- src/translator.py
+++ src/translator.py
@@ -6,7 +6,7 @@ import subprocess
 # process = subprocess.Popen(['ollama', 'serve'])
 
 # Get OLLAMA_HOST, if specified, or default to localhost:11434.
-MODEL_NAME = "mistral:7b"
+MODEL_NAME = "qwen3:0.6b"
 OLLAMA_URL = os.getenv("OLLAMA_HOST", "localhost:11434")
 
 client = Client(host=OLLAMA_URL)
